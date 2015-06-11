a91f7ab90008de177888a32f39998531b3bca3cc
MKHenson <mat@webinate.net>
2015-06-11 16:03:29 +0100

    Initial commit

---
+++ install-script.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -e
+{ # this ensures the entire script is downloaded #
+
+# Stops the execution of a script if a command or pipeline has an error
+set -e
+
+# Functiom that prints the latest stable version
+version() {
+  echo "0.0.1"
+}
+
+echo "cleaning up folder..."
+
+# Remove node modules
+if [ -d "node_modules" ]; then
+	rm node_modules -R
+fi
+
+# Remove views
+if [ -d "views" ]; then
+	rm views -R
+fi
+
+# Remove resources
+if [ -d "resources" ]; then
+	rm resources -R
+fi
+
+# Remove lib
+if [ -d "lib" ]; then
+	rm lib -R
+fi
+
+echo "Downloading latest version from github $(version)"
+
+#download latest
+wget https://github.com/MKHenson/noxy/archive/v$(version).zip
+unzip -o "v$(version).zip" "noxy-$(version)/*"
+
+# Moves the server folder to the current directory
+mv noxy-$(version)/server/* .
+
+# Remove noxy-master
+if [ -d "noxy-$(version)" ]; then
+	rm noxy-$(version) -R
+fi
+
+# Remove the zip file
+rm "v$(version).zip"
+
+if [ !-d "config.json" ]; then
+	# Copy the example config to a config.json
+	cp "example-config.json" "config.json"
+fi
+
+
+# All done
+echo "Noxy successfully installed"
+echo "Please run an NPM update and edit the config.json"
+exit
+} # this ensures the entire script is downloaded #
---
+++ src/lib/definitions/node-http-proxy.d.ts
@@ -0,0 +1,124 @@
+﻿// Type definitions for node-http-proxy
+// https://github.com/nodejitsu/node-http-proxy
+
+declare module "node-http-proxy" {
+
+    export interface ProxyServerOptions
+    {
+        /*
+        * url string to be parsed with the url module
+        */
+        target?: string;
+
+        /*
+        * url string to be parsed with the url module
+        */
+        forward?: string;
+
+        /*
+        * object to be passed to http(s).request
+        */
+        agent?: any;
+
+        /*
+        * object to be passed to https.createServer()
+        */
+        ssl?: any;
+
+        /*
+        * true/false, if you want to proxy websockets
+        */
+        ws?: boolean;
+
+        /*
+        * true/false, adds x-forward headers
+        */
+        xfwd?: boolean;
+
+        /*
+        * true/false, verify SSL certificate
+        */
+        secure?: boolean;
+
+        /*
+        * true/false, explicitly specify if we are proxying to another proxy
+        */
+        toProxy?: boolean;
+
+        /*
+        * true/false, Default: true - specify whether you want to prepend the target's path to the proxy path
+        */
+        prependPath?: boolean;
+
+        /*
+        * true/false, Default: false - specify whether you want to ignore the proxy path of the incoming request
+        */
+        ignorePath?: boolean;
+
+        /*
+        * Local interface string to bind for outgoing connections
+        */
+        localAddress?: string;
+
+        /*
+        * true/false, Default: false - changes the origin of the host header to the target URL
+        */
+        changeOrigin?: boolean;
+
+        /*
+        * Basic authentication i.e. 'user:password' to compute an Authorization header.  
+        */
+        auth?: string;
+
+        /*
+        * rewrites the location hostname on (301/302/307/308) redirects, Default: null.
+        */
+        hostRewrite?: any;
+
+        /*
+        * rewrites the location host/port on (301/302/307/308) redirects based on requested host/port. Default: false.
+        */
+        autoRewrite?: any;
+
+        /*
+        * rewrites the location protocol on (301/302/307/308) redirects to 'http' or 'https'. Default: null.
+        */
+        protocolRewrite?: any;
+    }
+
+    export interface Proxy
+    {
+        /**
+        * Sends a http(s) request
+        */
+        web(req, res, options?, callback?: Function);
+
+        /**
+        * Sends a websocket request
+        */
+        ws(req, socket, head, options?);
+
+        /**
+        * Makes the proxy listen on a port
+        */
+        listen(port: number);
+
+        /**
+        * When testing or running server within another program it may be necessary to close the proxy.
+        * This will stop the proxy from accepting new connections.
+        */
+        close();
+
+        /**
+        * Event listening
+        */
+        on(event: string, callback: Function);
+    }
+
+    /*
+    *  NOTE: `options.ws` and `options.ssl` are optional.
+    *  `options.target and `options.forward` cannot be
+    *  both missing
+    */
+    export function createProxyServer(options?: ProxyServerOptions): Proxy;
+}
