7c6dce1501647c8aad5b9be9ac380b17ab24ca1c
unknown <mat@webinate.net>
2015-10-13 17:20:45 +0100

    Updated to typescript 1.6 + added console colours

--- src/lib/Startup.ts
+++ src/lib/Startup.ts
@@ -6,14 +6,19 @@ import {IConfigFile} from "./Config";
 import * as winston from "winston";
 import * as yargs from "yargs";
 
-var arguments = yargs.argv;
+var args = yargs.argv;
+
+// Add the console colours
+winston.addColors({ debug: 'green', info: 'cyan', silly: 'magenta', warn: 'yellow', error: 'red' });
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, <any>{ level: 'debug', colorize: true });
 
 // Saves logs to file
-if (arguments.logFile && arguments.logFile.trim() != "")
-    winston.add(winston.transports.File, { filename: arguments.logFile, maxsize: 50000000, maxFiles: 1, tailable: true });
+if (args.logFile && args.logFile.trim() != "")
+    winston.add(winston.transports.File, <winston.TransportOptions>{ filename: args.logFile, maxsize: 50000000, maxFiles: 1, tailable: true });
 
 // If no logging - remove all transports
-if (arguments.logging && arguments.logging.toLowerCase().trim() == "false")
+if (args.logging && args.logging.toLowerCase().trim() == "false")
 {
     winston.remove(winston.transports.File);
     winston.remove(winston.transports.Console);
@@ -23,21 +28,21 @@ if (arguments.logging && arguments.logging.toLowerCase().trim() == "false")
 winston.info("Attempting to start up proxy server...", { process: process.pid });
 
 // Make sure the config path argument is there
-if (!arguments.config || arguments.config.trim() == "")
+if (!args.config || args.config.trim() == "")
 {
     winston.error("No config file specified. Please start noxy with the config path in the argument list. Eg: node Main.js --config=\"./config.js\"", { process: process.pid });
     process.exit();
 }
 
 // Make sure the file exists
-if (!fs.existsSync(arguments.config))
+if (!fs.existsSync(args.config))
 {
-    winston.error(`Could not locate the config file at '${arguments.config}'`, { process: process.pid });
+    winston.error(`Could not locate the config file at '${args.config}'`, { process: process.pid });
     process.exit();
 }
 
 // We have a valid file path, now lets try load it...
-var configPath: string = arguments.config;
+var configPath: string = args.config;
 var config: IConfigFile;
 
 try
