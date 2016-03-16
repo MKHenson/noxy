1e3211edb1d27e46784ceba7b0b19cd28e66fe3d
Mathew Henson <mat@webinate.net>
2016-03-16 11:01:50 +0000

    Added visual code & gulp files

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+// CONFIG
+// ==============================
+var outDir = "./server";
+
+// Builds each of the ts files into JS files in the output folder
+gulp.task('ts-code', function() {
+
+    return gulp.src(['src/lib/Main.ts', 'src/**/*.ts', 'src/**/*.json'], { base: "src/lib" })
+        .pipe(ts({
+            "module": "commonjs",
+            "removeComments": false,
+            "noEmitOnError": true,
+            "declaration": false,
+            "sourceMap": false,
+            "preserveConstEnums": true,
+            "target": "es5",
+            "noImplicitAny": false
+            }))
+        .pipe(gulp.dest(outDir));
+});
+
+gulp.task('build-all', ['ts-code']);
