0723e83f6f4b97ae45ad607174977e76c5b7ba16
Mathew Henson <mat@webinate.net>
2016-03-31 11:29:40 +0100

    Upgraded gulp and ts files with error checking and more consistency

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,40 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var fs = require('fs');
 
-// CONFIG
-// ==============================
-var outDir = "./server";
+var tsConfig = JSON.parse(fs.readFileSync('tsconfig.json'));
 
-// Builds each of the ts files into JS files in the output folder
-gulp.task('ts-code', function() {
+/**
+ * Checks to see that all TS files listed exist
+ */
+gulp.task('check-files', function(){
 
-    return gulp.src(['src/Main.ts', 'src/**/*.ts', 'src/**/*.json'], { base: "src/" })
+    // Make sure the files exist
+    for (var i = 0, l = tsConfig.files.length; i < l; i++ )
+        if(!fs.existsSync(tsConfig.files[i]))
+        {
+            console.log("File does not exist:" + tsConfig.files[i] );
+            process.exit();
+        }
+})
+
+/**
+ * Builds each of the ts files into JS files in the output folder
+ */
+gulp.task('ts-code', ['check-files'], function() {
+
+    return gulp.src(tsConfig.files, { base: "src/" })
         .pipe(ts({
-            "module": "commonjs",
-            "removeComments": false,
-            "noEmitOnError": true,
-            "declaration": false,
-            "sourceMap": false,
-            "preserveConstEnums": true,
-            "target": "es5",
-            "noImplicitAny": false
+            "module": tsConfig.compilerOptions.module,
+            "removeComments": tsConfig.compilerOptions.removeComments,
+            "noEmitOnError": tsConfig.compilerOptions.noEmitOnError,
+            "declaration": tsConfig.compilerOptions.declaration,
+            "sourceMap": tsConfig.compilerOptions.sourceMap,
+            "preserveConstEnums": tsConfig.compilerOptions.preserveConstEnums,
+            "target": tsConfig.compilerOptions.target,
+            "noImplicitAny": tsConfig.compilerOptions.noImplicitAny
             }))
-        .pipe(gulp.dest(outDir));
+        .pipe(gulp.dest(tsConfig.compilerOptions.outDir));
 });
 
 gulp.task('build-all', ['ts-code']);
