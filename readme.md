bc6f86d8c846915944d064fa8d8d1056eeac2341
Mathew Henson <mat@webinate.net>
2016-03-31 11:53:09 +0100

    Updated readme and config documentation

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # noxy
-Noxy is a node based proxy service, configurable through a config file
+Noxy is a node based proxy service, configurable through a config file. The underlying proxy module is based on (http-proxy)[https://github.com/nodejitsu/node-http-proxy]
+by nodejitsu.
 
 ## Current stable version
 * v0.1.1
@@ -48,4 +49,36 @@ can set the number of threads in the command line
 ```
 node Main.js --config="config.json" --numThreads="max" --logging="true" --logFile="logs.log"
 node Main.js --config="config.json" --numThreads="4" --logging="true" --logFile="logs.log"
+```
+
+## Setting up the paths
+
+You can find a detailed breakdown of all config options in the definition (file)[./src/definitions/custom/config.d.ts].
+Below is an example file that demonstrates a potential setup using a mixture of http endpoints as well as a web socket rule
+
+```
+{
+  "proxies": [
+    {
+      "port": 80,
+      "ssl": false,
+      "sslIntermediate": "",
+      "sslRoot": "",
+      "sslCert": "",
+      "sslKey": "",
+      "sslPassPhrase": "",
+      "sslPort": 443,
+      "routes": [
+        { "path": "domain-test\\.com", "target": "http://localhost:5001" },
+		{ "path": "admin\\.domain-test\\.com", "target": "http://localhost:8003" },
+        { "path": "api\\.domain-test\\.com", "target": "http://localhost:8002" },
+		{ "path": "test\\.net", "target": "http://localhost:8001", "redirects": true },
+		{ "path": "test\\.dev\/sockets", "target": "ws://localhost:3000", "isSocket": true },
+		{ "path": "test\\.dev\/api\/v1", "target": "http://localhost:5000" },
+		{ "path": "test\\.dev\/api\/v2", "target": "http://localhost:8000" },
+        { "path": "test\\.dev\/api\/v3", "target": "https://localhost:7000", "secure": true }
+      ]
+    }
+  ]
+}
 ```
--- src/definitions/custom/config.d.ts
+++ src/definitions/custom/config.d.ts
@@ -3,14 +3,74 @@ declare module Noxy
 {
     export interface IConfig
     {
+        /**
+         * The port noxy listens on for incomming requests
+         * Eg: 80
+         */
         port: number;
+
+        /**
+         * If true, noxy will setup both a regular and secure port.
+         * Eg: true/false
+         */
         ssl?: boolean;
+
+        /**
+         * The SSL intermediate file path. Only applicable if ssl is true
+         * Eg: './ssl/intermediate'
+         */
         sslIntermediate?: string;
+
+        /**
+         * The SSL root file path. Only applicable if ssl is true
+         * Eg: './ssl/root'
+         */
         sslRoot?: string;
+
+        /**
+         * The SSL certificate file path. Only applicable if ssl is true
+         * Eg: './ssl/cert'
+         */
         sslCert?: string;
+
+        /**
+         * The SSL key file path. Only applicable if ssl is true
+         * Eg: './ssl/key'
+         */
         sslKey?: string;
+
+        /**
+         * The SSL passphrase. Only applicable if ssl is true
+         * Eg: 'password'
+         */
         sslPassPhrase?: string;
+
+        /**
+         * The SSL port to listen on
+         * Eg: 443
+         */
         sslPort: number;
+
+        /**
+         * An array of routes and their respective proxy targets
+         *  - path: The regex string we use to test a requests
+         *  - target: The proxy target URI
+         *  - secure: A Boolean to indicate if the request must be secure or not
+         *  - redirects: A Boolean to indicate if noxy should redirect a request (sending a 302) as opposed to a pure proxy
+         *  - keepPathURI: A boolean to indicate if the proxy should maintain the URI (simply swap out the host with the target). Default is true.
+         *  - isSocket: A boolean to indicate if the route should be treated as a socket. Default is false.
+         * Eg:
+         * [
+         *   { "path": "domain-test\\.com", "target": "http://localhost:5001" },
+         *   { "path": "admin\\.domain-test\\.com", "target": "http://localhost:8003" },
+         *   { "path": "api\\.domain-test\\.com", "target": "http://localhost:8002" },
+         *   { "path": "test\\.net", "target": "http://localhost:8001", "redirects": true },
+         *   { "path": "test\\.dev\/sockets", "target": "ws://localhost:3000", "isSocket": true },
+         *   { "path": "test\\.dev\/api\/v1", "target": "http://localhost:5000" },
+         *   { "path": "test\\.dev\/api\/v2", "target": "http://localhost:8000" },
+         *   { "path": "test\\.dev\/api\/v3", "target": "https://localhost:7000", "secure": true }
+         * ]
+         */
         routes: Array<{
             path: string;
             target: string;
