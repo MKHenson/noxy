a5b55eecaf4ce7c9e16979a82ad6ae2a6aa9d943
Mathew Henson <mat@webinate.net>
2016-12-12 11:30:49 +0000

    Fixed const bug in log file generator and updated formatting

--- git-logs.js
+++ git-logs.js
@@ -1,46 +1,46 @@
-const util = require( 'util' )
-const fs = require( 'fs' )
-const exec = require( 'child_process' ).exec;
-const child;
+const util = require('util')
+const fs = require('fs')
+const exec = require('child_process').exec;
+let child;
 
 const prevTag = "v0.1.2";
 const nextTag = "v0.2.2";
 
 
 // Executes the git log command
-child = exec( 'git log ' + prevTag + '...' + nextTag + ' --pretty=format:"{ \\"author\\" : \\"%an\\", \\"commit\\" : \\"%h\\", \\"date\\" : \\"%ar\\", \\"title\\" : \\"%s\\" },"', function( error, stdout, stderr ) {
+child = exec('git log ' + prevTag + '...' + nextTag + ' --pretty=format:"{ \\"author\\" : \\"%an\\", \\"commit\\" : \\"%h\\", \\"date\\" : \\"%ar\\", \\"title\\" : \\"%s\\" },"', function(error, stdout, stderr) {
 
-    const jsonStr = "{ \"data\" : [ " + stdout.toString();
-    jsonStr = jsonStr.substr( 0, jsonStr.length - 1 ) + " ] } ";
+    let jsonStr = "{ \"data\" : [ " + stdout.toString();
+    jsonStr = jsonStr.substr(0, jsonStr.length - 1) + " ] } ";
 
     try {
-        const json = JSON.parse( jsonStr );
+        let json = JSON.parse(jsonStr);
         json = json.data;
 
-        console.log( "Parsed JSON" )
+        console.log("Parsed JSON")
 
-        const changes = "";
-        for ( let i = 0, l = json.length; i < l; i++ )
-            changes += `* ${json[ i ].title} - see ${json[ i ].commit} \n`;
+        let changes = "";
+        for (let i = 0, l = json.length; i < l; i++)
+            changes += `* ${json[i].title} - see ${json[i].commit} \n`;
 
         try {
-            fs.writeFile( 'changes.md', changes, function( err ) {
-                if ( err )
+            fs.writeFile('changes.md', changes, function(err) {
+                if (err)
                     throw err;
-                console.log( 'It\'s saved!' );
+                console.log('It\'s saved!');
             });
         }
-        catch ( err ) {
-            console.log( 'Could not write file:  ' + err );
+        catch (err) {
+            console.log('Could not write file:  ' + err);
         }
     }
-    catch ( err ) {
-        console.log( 'Could not parse JSON:  ' + err );
-        console.log( 'JSON STRING: ' + jsonStr );
+    catch (err) {
+        console.log('Could not parse JSON:  ' + err);
+        console.log('JSON STRING: ' + jsonStr);
     }
 
 
-    if ( error !== null ) {
-        console.log( 'exec error: ' + error );
+    if (error !== null) {
+        console.log('exec error: ' + error);
     }
 });
